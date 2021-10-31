fbf91a05c24ec88e21b93aa2164006c049e82674
Shy Shalom <shooshx@gmail.com>
2021-11-01 00:47:28 +0200

    added missing

--- infra/setup.py
+++ infra/setup.py
@@ -19,8 +19,10 @@ setup(
     zip_safe=False,
 )
 
-# pip install Pillow
-# pip install Cython
+# sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+
+# sudo apt-get install libsdl2-dev python3-dev
+# sudo pip3 install Pillow Cython pysdl2
 
 #  python setup.py build_ext --inplace
 
